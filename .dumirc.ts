c4972b4b8be816249003959ebb9ae9ca3e5d9a39
Lofipure <littlebanana@126.com>
2023-02-03 00:01:35 +0800

    feat: update dir name

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'dumi';
 
 export default defineConfig({
-  outputPath: 'docs-dist',
+  outputPath: 'docs',
+  // docDirs: ['docs-content'],
+  resolve: {
+    docDirs: ['docs-content'],
+  },
   themeConfig: {
     name: 'LofiInput',
   },
