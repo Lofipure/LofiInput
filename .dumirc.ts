e028d79cd476d202b7cd90e4af336aeb48c90e11
wangziheng.0915 <wangziheng.0915@bytedance.com>
2023-02-03 09:09:54 +0800

    feat: update config

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'dumi';
 
+const repo = 'LofiInput';
 export default defineConfig({
+  base: `/${repo}/`,
+  publicPath: `/${repo}/`,
   outputPath: 'docs-dist',
   themeConfig: {
     name: 'LofiInput',
+  },
+  resolve: {
+    docDirs: ['dist'],
   },
 });
